aacde2a5c45b406e53024454865c8ff9088e90d9
Łukasz Sikora <lukas.sikora@ab-date.de>
2024-11-11 11:20:42 +0800

    First upload

---
+++ FindInFile/Model/modelSearch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FindInFile.Model;
+
+public class modelSearch
+{
+   public string SearchText { get; set; }
+   public string Extension { get; set; }
+   public string Path { get; set; }
+   public bool SubDirs { get; set; }
+   public ObservableCollection<string> SearchPaths { get; set; }
+   public ObservableCollection<string> SearchExtensions { get; set; }
+   public ObservableCollection<string> SearchTexts { get; set; }
+   public UInt32 MaxPreViewSize { get; set; }
+   public string Title { get; set; }
+
+
+   public modelSearch()
+   {
+      SearchPaths = new ObservableCollection<string>();
+      SearchExtensions = new ObservableCollection<string>();
+      SearchTexts = new ObservableCollection<string>();
+   }
+
+}
